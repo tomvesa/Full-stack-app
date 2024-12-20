01c6da4ae0eee0dce1ac5f0515d3f77dcfdf74bd
tomvesa <47905734+tomvesa@users.noreply.github.com>
2024-12-20 01:24:42 +0100

    Add link to Header Title

--- client2/src/components/Header.jsx
+++ client2/src/components/Header.jsx
@@ -1,13 +1,17 @@
-
+import { useNavigate } from "react-router-dom";
 
 import Nav from "./Nav.jsx";
 
 const Header = () => {
+    const navigate = useNavigate();
 
+    const returnToHome = () => {
+      navigate("/");
+    }
   
     return (
       <header className="header--flex" >
-          <h1 className="header--logo">Courses</h1>
+          <h1 className="header--logo" onClick={returnToHome} >Courses</h1>
           <Nav />
       </header>
     );
