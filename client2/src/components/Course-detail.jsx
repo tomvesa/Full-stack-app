831172008c67dbbd2428a65a90cd1215f3e77afe
tomvesa <47905734+tomvesa@users.noreply.github.com>
2024-12-20 01:08:48 +0100

    Set Delete Api

--- client2/src/components/Course-detail.jsx
+++ client2/src/components/Course-detail.jsx
@@ -16,10 +16,12 @@ const CourseDetail = () => {
 
     const [courseData, setCourseData] = useState(null);
     const [courseMaterials, setCourseMaterials] = useState([]);
+    const [success, setSuccess] = useState(null);
+    const [errors, setErrors] = useState([]);
     // Get path name without "/" to be used in the API call
     const path = location.pathname.substring(1);
 
-    
+    // call API to get the course data
     const callAPI = async () => {
         try {
             const response = await api(`${path}`);
@@ -37,13 +39,40 @@ const CourseDetail = () => {
         }
     };
 
+    // call API to delete course
+    const callDeleteAPI = async() =>{
+        try {
+            const response = await api(`${path}`, "DELETE");
+            console.log(response);
+            if (response.status === 204) {
+                setSuccess("Course deleted successfully");
+                setTimeout(() =>{
+                    navigate("/");
+                }, 2000);
+            } else if (response.status === 401){
+                setErrors(["Not authorized to delete this course"]);
+                setTimeout(() =>{
+                    navigate("/");
+                }, 2000);
+            } else {
+                setErrors(["Course not found"]);
+                setTimeout(() =>{
+                    navigate("/404");
+                }, 2000);
+            }
+        } catch (error) {
+            console.error('Error deleting course:', error);
+            navigate("/404");
+        }
+    }
+
     useEffect(() => {
         callAPI();
     }, []);
 
     useEffect(() => {
         if (courseData) {
-            console.log(courseData); // Will log after course is updated
+            console.log(courseData); 
             if (courseData.materialsNeeded) {
             const materials = courseData.materialsNeeded.split('\n').map(material => material.replace("*","").trim())
             setCourseMaterials( materials ) 
@@ -63,46 +92,60 @@ const CourseDetail = () => {
         navigate(`/${path}/update`);
     }
 
+    const handleDelete = (e) => {
+        e.preventDefault();
+        callDeleteAPI()
+    }
+
     return (
-        <main>
+      <main>
         {/* display action bar only when the course is created by logged in user */}
-         {  user.id === courseData.userId ? 
-            <div className="actions--bar">
-                <div className="wrap">
-                    <a className="button" onClick={handleUpdate}>Update Course</a>
-                    <a className="button" href="#">Delete Course</a>
-                    <a className="button button-secondary" onClick={handleReturn}>Return to List</a>
-                </div>
-            </div>
-            : null }
+        {user.id === courseData.userId ? (
+          <div className="actions--bar">
             <div className="wrap">
-                <h2>Course Detail</h2>
-                <form>
-                    <div className="main--flex">
-                        <div>
-                            <h3 className="course--detail--title">Course</h3>
-                            <h4 className="course--name">{courseData.title}</h4>
-                            <p>By {courseData.Instructor?.firstName} {courseData.Instructor?.lastName}</p>
-                            <p className="course--description">{courseData.description}</p>
-                        </div>
-                        <div>
-                            <h3 className="course--detail--title">Estimated Time</h3>
-                            <p>{courseData.estimatedTime || "N/A"}</p>
-
-                            <h3 className="course--detail--title">Materials Needed</h3>
-                            <ul className="course--detail--list">
-                            {courseMaterials.map((material, index) => (
-                                    material ?
-                                    <li key={index}>{material}</li>:
-                                    ""                                    
-                                ))}
-
-                            </ul>
-                        </div>
-                    </div>
-                </form>
+              <a className="button" onClick={handleUpdate}>
+                Update Course
+              </a>
+              <a className="button" onClick={handleDelete}>
+                Delete Course
+              </a>
+              <a className="button button-secondary" onClick={handleReturn}>
+                Return to List
+              </a>
             </div>
-        </main>
+          </div>
+        ) : null}
+        <div className="wrap">
+          <h2>Course Detail</h2>
+          <div className="notification">
+            {success && <span className="success">{success}</span>}
+          </div>
+          <form>
+            <div className="main--flex">
+              <div>
+                <h3 className="course--detail--title">Course</h3>
+                <h4 className="course--name">{courseData.title}</h4>
+                <p>
+                  By {courseData.Instructor?.firstName}{" "}
+                  {courseData.Instructor?.lastName}
+                </p>
+                <p className="course--description">{courseData.description}</p>
+              </div>
+              <div>
+                <h3 className="course--detail--title">Estimated Time</h3>
+                <p>{courseData.estimatedTime || "N/A"}</p>
+
+                <h3 className="course--detail--title">Materials Needed</h3>
+                <ul className="course--detail--list">
+                  {courseMaterials.map((material, index) =>
+                    material ? <li key={index}>{material}</li> : ""
+                  )}
+                </ul>
+              </div>
+            </div>
+          </form>
+        </div>
+      </main>
     );
 };
 
