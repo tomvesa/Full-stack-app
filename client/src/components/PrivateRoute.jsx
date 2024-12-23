b8b49df844aba3f859f9942dc59a1987bcc473ea
tomvesa <47905734+tomvesa@users.noreply.github.com>
2024-12-23 19:17:50 +0100

    RF redirect from signin to private route based on original address

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,11 +1,13 @@
 import { useContext } from "react";
-import {Navigate, Outlet } from "react-router-dom";
+import {Navigate, Outlet, useLocation } from "react-router-dom";
 import UserContext from "../context/UserContext";
 
 const PrivateRoute = () => {
     const { authUser} = useContext(UserContext);
+    const location = useLocation()
+    console.log(location);
 // user private route if user is authenticated, else redirect to signin
-   return  authUser ? <Outlet /> : <Navigate to="users/signin" />;
+   return  authUser ? <Outlet /> : <Navigate to="users/signin" state={{from: location.pathname}} />;
 }
 
 export default PrivateRoute;
--- client/src/components/User-Sign-in.jsx
+++ client/src/components/User-Sign-in.jsx
@@ -1,4 +1,4 @@
-import {useNavigate, Link} from 'react-router-dom';
+import {useNavigate, Link, useLocation} from 'react-router-dom';
 import { useRef, useContext, useState } from 'react';
 import ValidationErrors from './ValidationError';
 import UserContext from "../context/UserContext";
@@ -6,15 +6,24 @@ import UserContext from "../context/UserContext";
 const UserSignIn = () => {
     const {actions} = useContext(UserContext);
     const navigate = useNavigate();
+    const location = useLocation();
+    console.log(location)
 
     const [errors, setErrors] = useState([]);
     // form fields state
     const emailAddress = useRef('');
     const password = useRef('');
 
+    const handleCancel = ()=>{
+        navigate("/");
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+        let from = "/";
+        if(location.state){
+            from = location.state.from;
+        }
         const  credentials = {
             emailAddress: emailAddress.current.value,
             password: password.current.value
@@ -24,7 +33,7 @@ const UserSignIn = () => {
             const user = await actions.signIn(credentials);   
            
             if (user) {
-                navigate("/");
+                navigate(from);
             } else {
                 setErrors(["Sign in failed"])
             } 
@@ -49,7 +58,7 @@ const UserSignIn = () => {
                     <input id="password" name="password" type="password" ref={password} />
 
                     <button className="button" type="submit">Sign In</button>
-                    <button className="button button-secondary">Cancel</button>
+                    <button className="button button-secondary" onClick={handleCancel}>Cancel</button>
                 </form>
                 <p>{`Don't have a user account? Click here to `}<Link to="/users/signup">sign up</Link>!</p>
                 
